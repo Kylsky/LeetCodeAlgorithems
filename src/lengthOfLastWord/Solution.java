fc879656baf5f21a998febf06efef31af2ab2f87
kongyilei <752051085@qq.com>
2020-11-11 10:23:25 +0800

    No.58 length-of-last-word

---
+++ src/lengthOfLastWord/Solution.java
@@ -0,0 +1,31 @@
+package lengthOfLastWord;
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+//            s = s.trim();
+//            int start = s.lastIndexOf(" ") + 1;
+//            return s.substring(start).length();
+
+        // 不使用indexOf和subString的方法
+        int length = 0;
+        boolean flag = true;
+        for (char by : s.toCharArray()) {
+            if (by==' ') {
+                flag = false;
+            } else if (flag) {
+                length++;
+            } else {
+                length = 0;
+                length++;
+                flag = true;
+            }
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLastWord("Hello World"));
+        ;
+    }
+}
