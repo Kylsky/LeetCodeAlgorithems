f9f6b117de24a6a8da4247444f64deae6525ea8a
kongyilei <752051085@qq.com>
2020-11-18 09:02:09 +0800

    No.136 single-number

---
+++ src/single_number/Solution.java
@@ -0,0 +1,11 @@
+package single_number;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            i ^= nums[j];
+        }
+        return i;
+    }
+}
