9eaec202b31b1d55a094dfb7787a6de2327e46cc
kongyilei <752051085@qq.com>
2020-11-18 08:48:18 +0800

    堆排序相关

--- src/heap/HeapExtend.java
+++ src/heap/HeapExtend.java
@@ -4,7 +4,7 @@ import java.util.PriorityQueue;
 
 public class HeapExtend {
     public static void main(String[] args) {
-        int[] arr = new int[]{3, 1, 2, 6, 4, 5};
+        int[] arr = new int[]{1, 1, 2, 6, 4, 5};
 
         sort(arr, 3);
         for (int i = 0; i < arr.length; i++) {
@@ -13,27 +13,38 @@ public class HeapExtend {
     }
 
     public static int[] sort(int[] arr, int k) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue(arr.length);
+        PriorityQueue<Integer> heap = new PriorityQueue<>(arr.length);
         int i = 0;
-        for (; i <= Math.min(k,arr.length-1); i++) {
-            priorityQueue.add(arr[i]);
+        for (;i<=Math.min(arr.length-1,k);i++){
+            heap.add(arr[i]);
         }
-        int index = 0;
-        for (; i<arr.length;index++,i++) {
-            priorityQueue.add(arr[i]);
-            arr[index] = priorityQueue.poll();
+        int index= 0 ;
+        for(;i<arr.length;i++,index++){
+            arr[index] = heap.poll();
+            heap.add(arr[i]);
         }
-        while (!priorityQueue.isEmpty()){
-            arr[index++] = priorityQueue.poll();
+        while (!heap.isEmpty()){
+            arr[index++] = heap.poll();
         }
-
-
         return arr;
     }
 
-    public static void swap(int[] arr, int a, int b) {
-        int tmp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = tmp;
-    }
+//    public static int[] sort(int[] arr, int k) {
+//        PriorityQueue<Integer> priorityQueue = new PriorityQueue(arr.length);
+//        int i = 0;
+//        for (; i <= Math.min(k,arr.length-1); i++) {
+//            priorityQueue.add(arr[i]);
+//        }
+//        int index = 0;
+//        for (; i<arr.length;index++,i++) {
+//            priorityQueue.add(arr[i]);
+//            arr[index] = priorityQueue.poll();
+//        }
+//        while (!priorityQueue.isEmpty()){
+//            arr[index++] = priorityQueue.poll();
+//        }
+//
+//
+//        return arr;
+//    }
 }
